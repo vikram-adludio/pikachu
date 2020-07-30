da447cc368e144e89c7bde220cbf731962172824
Vikram Bageja <vikram.bageja@adludio.com>
2020-07-30 20:55:43 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import pikachu from './assets/pikachu-6.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello world</h1>
+      <img src={pikachu} alt='dsdas'></img>
     </div>
   );
 }
